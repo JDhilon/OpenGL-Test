96ac57acf29e9bb185416b4a6a4466adc459710f
Jashanjot Dhilon <jashanjotdhilon@gmail.com>
2019-11-28 13:10:43 -0700

    Initial Commit

---
+++ OpenGL-Test/main.cpp
@@ -0,0 +1,27 @@
+//
+//  mesh.hpp
+//  OpenGL-Test
+//
+//  Created by Jashanjot Dhilon on 2019-11-26.
+//  Copyright © 2019 Jashanjot Dhilon. All rights reserved.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main()
+{
+    return 0;
+}
+
