a233617d97e6ce7a5e8ef0d0088772e9c16c22ee
Jashanjot Dhilon <jashanjotdhilon@gmail.com>
2019-11-28 13:41:46 -0700

    Added translation support in shader file.

--- OpenGL-Test/main.cpp
+++ OpenGL-Test/main.cpp
@@ -22,7 +22,12 @@
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VBO, VAO, shader;
+GLuint VBO, VAO, shader, uniformXMove;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.0005f;
 
 // Vertex Shader code
 static const char* vShader = "                                                \n\
@@ -30,9 +35,11 @@ static const char* vShader = "                                                \n
 \n\
 layout (location = 0) in vec3 pos;                                              \n\
 \n\
+uniform float xMove;                                                          \n\
+\n\
 void main()                                                                   \n\
 {                                                                             \n\
-gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);                  \n\
+gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);                  \n\
 }";
 
 // Fragment Shader
@@ -130,6 +137,7 @@ void CompileShaders()
         return;
     }
     
+    uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 int main()
@@ -190,11 +198,26 @@ int main()
         // Get + Handle user input events
         glfwPollEvents();
         
+        if (direction)
+        {
+            triOffset += triIncrement;
+        }
+        else {
+            triOffset -= triIncrement;
+        }
+        
+        if (abs(triOffset) >= triMaxOffset)
+        {
+            direction = !direction;
+        }
+        
         // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         glUseProgram(shader);
+        
+        glUniform1f(uniformXMove, triOffset);
         
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -207,4 +230,3 @@ int main()
     
     return 0;
 }
-
