82536c17f64f5d0161fba09d5c8b1449b96d533a
Jashanjot Dhilon <jashanjotdhilon@gmail.com>
2019-11-28 13:44:59 -0700

    Added glm to handle transforms via matrices

--- OpenGL-Test/main.cpp
+++ OpenGL-Test/main.cpp
@@ -21,13 +21,21 @@
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VBO, VAO, shader, uniformXMove;
+GLuint VBO, VAO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxOffset = 0.7f;
 float triIncrement = 0.0005f;
+
+float curAngle = 0.0f;
+
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
 
 // Vertex Shader code
 static const char* vShader = "                                                \n\
@@ -35,22 +43,22 @@ static const char* vShader = "                                                \n
 \n\
 layout (location = 0) in vec3 pos;                                              \n\
 \n\
-uniform float xMove;                                                          \n\
+uniform mat4 model;                                                           \n\
 \n\
 void main()                                                                   \n\
 {                                                                             \n\
-gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);                  \n\
+gl_Position = model * vec4(pos, 1.0);                                      \n\
 }";
 
 // Fragment Shader
 static const char* fShader = "                                                \n\
 #version 330                                                                  \n\
 \n\
-out vec4 colour;                                                               \n\
+out vec4 colour;                                                              \n\
 \n\
 void main()                                                                   \n\
 {                                                                             \n\
-colour = vec4(1.0, 0.0, 0.0, 1.0);                                         \n\
+colour = vec4(1.0, 0.0, 0.0, 1.0);                                        \n\
 }";
 
 void CreateTriangle()
@@ -137,7 +145,7 @@ void CompileShaders()
         return;
     }
     
-    uniformXMove = glGetUniformLocation(shader, "xMove");
+    uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main()
@@ -211,13 +219,41 @@ int main()
             direction = !direction;
         }
         
+        curAngle += 0.01f;
+        if (curAngle >= 360)
+        {
+            curAngle -= 360;
+        }
+        
+        if (direction)
+        {
+            curSize += 0.0001f;
+        }
+        else {
+            curSize -= 0.0001f;
+        }
+        
+        if (curSize >= maxSize || curSize <= minSize)
+        {
+            sizeDirection = !sizeDirection;
+        }
+        
         // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         glUseProgram(shader);
         
-        glUniform1f(uniformXMove, triOffset);
+        glm::mat4 model = glm::mat4(1.0f);
+        
+        //model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+        
+        model = glm::scale(model, glm::vec3(curSize, 0.4f, 0.0f));
+        
+        
+        
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
         
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
